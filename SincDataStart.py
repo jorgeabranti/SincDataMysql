3031645b32a563a061e096db515f58a06df056e1
JORGEABRANTI <jorgeabranti@gmail.com>
2018-12-10 21:01:30 -0200

    adicionado tipo de valores no comentario do json

--- SincDataStart.py
+++ SincDataStart.py
@@ -1,15 +1,15 @@
 from APP.SincDataThreading import SincDataThreading
 """                                !!!LEIA COM ATENÇÃO!!!  
-*tables: especifica as tabelas que devem ter os dados migrados
-*table_name: nome da tabela a ter os dados migrados
-*order_by_date: nome da coluna 'date' utilizada nas cláusulas order by e para controle do período de cópia
-*pk_table: nome da coluna chave primária da tabela
-*connection_prod: dados para conexão com a base de produção
-*connection_bh: dados para conexão com a base histórica
-*time_start: horário para início da cópia
-*time_end: horário para fim da cópia
-*days_in_prod: quantidade de dias para manter na base de produção
-*days_keep_log: quantidade de dias para manter os arquivos de log
+*tables(string): especifica as tabelas que devem ter os dados migrados
+*table_name(string): nome da tabela a ter os dados migrados
+*order_by_date(string): nome da coluna 'date' utilizada nas cláusulas order by e para controle do período de cópia
+*pk_table(string): nome da coluna chave primária da tabela
+*connection_prod(string): dados para conexão com a base de produção
+*connection_bh(string): dados para conexão com a base histórica
+*time_start(string): horário para início da cópia
+*time_end(string): horário para fim da cópia
+*days_in_prod(string): quantidade de dias para manter na base de produção
+*days_keep_log(int): quantidade de dias para manter os arquivos de log
 """
 __imports = {"tables": [
                 {"table_name": "chamadas", "order_by_date": "calldate", "pk_table": "id_chamada"},
